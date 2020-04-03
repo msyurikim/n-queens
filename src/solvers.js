50f7076ec770cfad07b1e4243cfe7965bfc617f5
Yuri Kim <yurikim123@gmail.com>
2020-04-02 15:37:53 -1000

    completed findNQueensSolution

--- src/solvers.js
+++ src/solvers.js
@@ -35,13 +35,13 @@ window.findNRooksSolution = function(n) {
 window.countNRooksSolutions = function(n) {
   var solutionCount = 0;
 
-  if (n == 1) {
+  if (n === 1) {
     solutionCount = 1;
   } else if (n === 2) {
     solutionCount = 2;
   } else {
     for (var i = 0; i < n; i++) {
-      solutionCount += countNRooksSolutions(n-1);
+      solutionCount += countNRooksSolutions(n - 1);
     }
   }
 
@@ -51,53 +51,52 @@ window.countNRooksSolutions = function(n) {
 
 // return a matrix (an array of arrays) representing a single nxn chessboard, with n queens placed such that none of them can attack each other
 window.findNQueensSolution = function(n) {
-  var solution = [];
+  var board = new Board({n: n});
 
-  //console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
+  var callback = function() {
+    return board.rows().map(function(row) {
+      return row.slice();
+    });
+  };
+
+  var solution = findSolution(0, n, board, callback) || board.rows();
+
+  console.log('Single solution for ' + n + ' queens:', JSON.stringify(solution));
   return solution;
 };
 
 // return the number of nxn chessboards that exist, with n queens placed such that none of them can attack each other
 window.countNQueensSolutions = function(n) {
   var solutionCount = 0;
-  var matrix = [];
-  for (var i = 0; i < n; i++) {
-    matrix[i] = [];
-    for (var j = 0; j < n; j++) {
-      matrix[i][j] = 0;
-    }
-  }
   //instantiation rule in Board.js line 12, for empty board
-  var board = new Board(matrix);
+  var board = new Board({n: n});
 
-  var findSolution = function(row) {
-
-    if (row === n) {
-      solutionCount++;
-      return;
-    }
-
-    //iterating through all columns for row
-    for (var i = 0; i < n; i++) {
-      //place piece
-      board.togglePiece(row, i);
-      if(!board['hasAnyQueenConflictsOn']()){
-        //no conflicts, move to next row using recursion
-        //keep recursing, if no conflict
-        findSolution(row+1);
-      }
-      //else {
-      //remove piece
-        board.togglePiece();
-    //}
-
-    }
-
+  //start from first row
+  var callback = function() {
+    solutionCount++;
   };
-
-  findSolution(0); //start from first row
-
-
+  findSolution(0, n, board, callback);
   console.log('Number of solutions for ' + n + ' queens:', solutionCount);
   return solutionCount;
+};
+
+window.findSolution = function(row, n, board, callback) {
+  if (row === n) {
+    return callback();
+  }
+  //iterating through all columns for row
+  for (var i = 0; i < n; i++) {
+    //place piece
+    board.togglePiece(row, i);
+    if (!board['hasAnyQueensConflicts']()) {
+      //no conflicts, move to next row using recursion
+      //keep recursing, if no conflict
+      var result = findSolution(row + 1, n, board, callback);
+      if (result) {
+        return result;  //eject out of recursion
+      }
+    }
+    //remove piece, will go to next column, same row in next loop
+    board.togglePiece(row, i);
+  }
 };
